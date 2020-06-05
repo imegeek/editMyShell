84fa926014c11c062a3e6ea78451f1c2ccaac2e7
ABHacker Official <63346676+abhackerofficial@users.noreply.github.com>
2020-06-05 17:47:59 +0530

    Update t-edit.sh

--- t-edit.sh
+++ t-edit.sh
@@ -1,5 +1,28 @@
+#!/bin/bash
 echo -e '\033[1m\033[38;2;200;0;150m
 ┏━━┓ ┏━┓┏┳┳┓
 ┗┓┏┻━┫┳╋┛┣┫┗┓
  ┃┣━━┫┻┫╋┃┃┏┫
  ┗┛  ┗━┻━┻┻━┛'
+echo
+printf "\e[1;92m[\e[0m\e[1;77m1\e[0m\e[1;92m]\e[0m\e[1;92m bash.bashrc"
+echo
+echo
+printf "\e[1;92m[\e[0m\e[1;77m2\e[0m\e[1;92m]\e[0m\e[1;92m zshrc"
+echo
+printf $'\n\e[1;91m[\e[0m\e[1;77m!\e[0m\e[1;91m] Choose an option: \e[0m'
+read nano
+if [[ $nano = "1" ]] || [[ $nano = "01" ]]
+        then clear
+cd .nano && bash bash.bashrc
+
+elif [[ $nano = "2" ]] || [[ $nano = "02" ]]
+        then clear
+cd .nano && bash zshrc
+
+else
+echo
+printf "\e[1;91m[!] Invalid option! ~Restarting..\e[0m\n"
+sleep 1
+bash t-edit.sh
+fi
